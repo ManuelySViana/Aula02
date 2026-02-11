78714bfabb3eff629d524fd74207ee75018716e5
MANUELY DA SILVA VIANA <25162431@edu119.local>
2026-02-11 15:58:56 -0300

    Versao 2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <main>
+      <h1>Bem-vindo</h1>
+      <p>Entre com seu e-mail e senha para continuar</p>
+
+      <input type="email" placeholder="Digite seu e-mail" />
+      <input type="password" placeholder="Digite sua senha" />
+
+      <button>Entrar</button>
+    </main>
+  );
+}
